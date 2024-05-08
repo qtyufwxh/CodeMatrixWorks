6b91c8a7ceb7c72728abd8e5ad38af5b26bdcae7
qtyufwxh <gs1manu8909@hotmail.com>
2024-05-08 16:46:11 +0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const sum = (a, b) => a + b;
+const binarySearchIterative = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
