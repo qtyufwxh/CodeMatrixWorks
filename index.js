16446cca0b26b407f94bfd029f697bf621692fe3
qtyufwxh <gs1manu8909@hotmail.com>
2024-05-11 00:34:55 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function reverseList(head) {
-  let prev = null;
-  let curr = head;
-  while (curr) {
-    const next = curr.next;
-    curr.next = prev;
-    prev = curr;
-    curr = next;
+function maxEnvelopes(envelopes) {
+  envelopes.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+  const dp = new Array(envelopes.length).fill(1);
+  let max = 1;
+  for (let i = 1; i < envelopes.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (envelopes[i][1] > envelopes[j][1]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+        max = Math.max(max, dp[i]);
+      }
+    }
   }
-  return prev;
+  return max;
 }
